2d2c39a6979ec65478fa91eb21a38b8c6ad14a2a
Marko Harjula <marko.harjula@avanio.com>
2025-04-24 06:13:20 +0300

    doc updates

--- src/types/Edge.ts
+++ src/types/Edge.ts
@@ -1,3 +1,8 @@
 import {type IGraphEntityNode} from './Node';
 
+/**
+ * A graph edge connecting two nodes and is a tuple of two nodes.
+ * @template Entity The type of the node.
+ * @since v0.0.7
+ */
 export type GraphEdge<Entity extends IGraphEntityNode<number, Record<string, unknown>>> = [node1: Entity, node2: Entity];
